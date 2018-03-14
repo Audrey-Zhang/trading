ed891d2622651db6add4496aaab6e59575e444fe
Audrey_zhang <ariesyun@163.com>
2018-03-14 23:48:37 +0800

    update tick download task

--- Usual Task.py
+++ Usual Task.py
@@ -8,6 +8,7 @@ Created on Tue Nov 28 21:19:36 2017
 import pandas as pd
 import tushare as ts  
 import datetime  
+import time 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -20,15 +21,36 @@ cnx = engine.connect()
 # ============my lib===============
 import dtDownload as dtd
 
+def update_retrun_list():
+    '''
+    return current Stock_ID list  --ll
+    insert new Stock_ID to db_status
+    '''
+    df_l = ts.get_stock_basics()  # generally use 2s
+    time.sleep(5)
+    ll = df_l.index.tolist()
+    sql_str = 'SELECT Stock_ID from db_status'
+    df_exist = pd.read_sql(sql_str,cnx)
+    s1 = set(df_l.index);
+    s2 = set(df_exist['Stock_ID'])
+    new = s1 - s2
+    print(len(new))
+    df_new = df_l.loc[new,['name','industry']]
+    df_new['Stock_ID'] = df_new.index
+    df_new.to_sql('db_status', cnx, if_exists='append', index=False)
+    return ll
+
+sl = update_retrun_list()
+
 dts = '2018-01-01'
-dte = '2018-03-07'
+dte = '2018-01-03'
 sbL = ['300231','300290','002279','603138','603881','300608',
        '300609','300166','300541','300245','000717','603322',
        '000004','002288','002225','002886','002310','600516',
        '000735','002320','600555','000886','600717','002177']
 #sbL = ['002177']
 
-dtd.dl_Tick(dts,dte,sbL)
+dtd.dl_Tick(dts,dte,sl)
 #dtd.gen_D(sbL)
 '''
 import dtDownload as dtd
